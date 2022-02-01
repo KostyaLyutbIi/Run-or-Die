dc2c78cd930d3f962de7af59e64ca042f00bce1d
Konstantin <lyutbii@icloud.com>
2022-02-01 17:49:30 +0300

    The enemy is chasing

--- Assets/Scripts/EnemyAgent.cs
+++ Assets/Scripts/EnemyAgent.cs
@@ -3,17 +3,30 @@ using UnityEngine.AI;
 
 public class EnemyAgent : MonoBehaviour
 {
-    public Transform player;
-    public NavMeshAgent agent;
 
-    void Start()
+    public Transform player;
+
+    private NavMeshAgent _agent;
+    private float _distance = 15;
+    private float _radius = 10;
+
+    void Start() 
     {
-        agent = GetComponent<NavMeshAgent>();
-        agent.destination = player.position;
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.destination = player.position;
     }
 
     private void Update()
     {
-        agent.SetDestination(player.position);
+        _distance = Vector3.Distance(player.transform.position, transform.position);
+
+        if (_distance > _radius)
+            _agent.enabled = false;
+
+        if (_distance < _radius)
+        {
+            _agent.enabled = true;
+            _agent.SetDestination(player.transform.position);
+        }
     }
 }
